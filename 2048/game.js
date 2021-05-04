ab201368267f5fc1483e67b17fe03391b9e3001a
Balazs Toth <tothbalazs.contact@gmail.com>
2021-05-04 15:49:57 +0200

    Add keypress event listeners for the arrow keys

--- 2048/game.js
+++ 2048/game.js
@@ -1,3 +1,25 @@
 'use strict';
 
-import grid from './modules/grid';
+import grid from './modules/grid.js';
+
+grid.init();
+
+document.addEventListener('keyup', function (event) {
+    let direction = null;
+
+    if (event.key === 'ArrowUp') {
+        direction = 'UP';
+    } else if (event.key === 'ArrowRight') {
+        direction = 'RIGHT';
+    } else if (event.key === 'ArrowLeft') {
+        direction = 'LEFT';
+    } else if (event.key === 'ArrowDown') {
+        direction = 'DOWN';
+    }
+
+    if (direction !== null) {
+        grid.slide(direction);
+    }
+
+    return false;
+});
