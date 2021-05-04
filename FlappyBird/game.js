bc6f144612ff1a8b4d0d8c42292283d2bf413f26
HuBori <hunyadyb2000@gmail.com>
2021-05-04 17:50:55 +0200

    some background for pipes (doesn't work yet)

--- FlappyBird/game.js
+++ FlappyBird/game.js
@@ -8,6 +8,33 @@
 //
 // }
 
+function makeNew(counter)
+{
+    var random = Math.random() % 400;
+    document.body.appendChild( document.createElement('div') );
+    counter++;
+    for (cl of ["lower", "upper"])
+    {
+        var new_pipe = document.createElement('div');
+        new_pipe.setAttribute("class", '.'+cl+'_pipe');
+        new_pipe.setAttribute("id", `${cl}_pipe_${counter}`);
+        document.body.appendChild(new_pipe);
+    }
+
+    var upper = document.querySelector('#upper_pipe_' + counter.toString());
+    var lower = document.querySelector('#lower_pipe_' + counter.toString());
+
+    upper.bottom = random;
+    lower.top = random;
+}
+
+function PlacePipes()
+{
+    var timer = 1000;
+    var counter = 0;
+    setInterval(timer = function(timer){timer -= 200; return timer;}, 10000);
+    setTimeout(function(){setInterval(makeNew(counter), timer)}, 3000);
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     const bird = document.querySelector('.bird');
@@ -28,6 +55,7 @@ document.addEventListener('DOMContentLoaded', () => {
         time = Date.now();
         bird.style.bottom = birdBottom + 'px';
         bird.style.left = birdLeft + 'px';
+        PlacePipes() // Bori
     }
     let timerId = setInterval(startGame, interval);
 
