d004615f8a75d339c22165202ff7e0a7ab6c1115
Balazs Toth <tothbalazs.contact@gmail.com>
2021-05-04 09:44:08 +0200

    Function for jumping with the bird object

--- game.js
+++ game.js
@@ -1,7 +1,34 @@
-initGame();
+'use strict';
 
-function initGame() {
+// initGame();
+//
+// function initGame() {
+//
+//     // Your game can start here, but define separate functions, don't write everything in here :)
+//
+// }
 
-    // Your game can start here, but define separate functions, don't write everything in here :)
 
-}
+document.addEventListener('DOMContentLoaded', () => {
+    const bird = document.querySelector('.bird');
+    const gameDisplay = document.querySelector('.game-container');
+    const ground = document.querySelector('.ground');
+
+    let birdLeft = 220;
+    let birdBottom = 100;
+    let gravity = 2;
+
+    function startGame() {
+        birdBottom -= gravity
+        bird.style.bottom = birdBottom + 'px';
+        bird.style.left = birdLeft + 'px';
+    }
+    let timerId = setInterval(startGame, 20);
+
+
+    function jump() {
+        birdBottom += 50
+        bird.style.bottom = birdBottom + 'px';
+    }
+    document.addEventListener('keyup', jump);
+})
