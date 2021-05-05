95bf08d8603aa9b04b69f4d2c264f30d29665e8a
prothy <lukacsurgay@gmail.com>
2021-05-05 10:07:02 +0200

    some refactoring

--- FlappyBird/game.js
+++ FlappyBird/game.js
@@ -1,12 +1,49 @@
 'use strict';
 
-// initGame();
-//
-// function initGame() {
-//
-//     // Your game can start here, but define separate functions, don't write everything in here :)
-//
-// }
+
+document.addEventListener('DOMContentLoaded', () => {
+    startGame()
+})
+
+function startGame() {
+    const bird = document.querySelector('.bird');
+    const gameDisplay = document.querySelector('.game-container');
+    const ground = document.querySelector('.ground');
+
+
+    /* PHSYICS CONSTANTS */
+    const gravity = -150   // gravitational acceleration
+    let velocity = 0;      // vertical velocity
+    let initTime = Date.now()
+    const interval = 20    // milliseconds per update
+    
+    
+    let gameStarted = false;
+
+    let birdBottom = 100;
+
+    function gameLoop() {
+        let curTime = Date.now();
+        let timeDiff = (curTime - initTime)/1000
+        if (birdBottom > 0 && gameStarted) birdBottom += (velocity * timeDiff) + (gravity * timeDiff**2)/2
+        bird.style.bottom = birdBottom + 'px';
+        PlacePipes() // Bori
+        // if (birdBottom === 0) break;
+        document.addEventListener('keydown', jump);
+    }
+    let timerId = setInterval(gameLoop, interval);
+
+
+    function jump(e) {
+        if (e.key == ' ') {
+            if (!gameStarted) gameStarted = true;
+
+            initTime = Date.now();
+            velocity = 40;
+            bird.style.bottom = birdBottom + 'px';
+        }
+    }
+}
 
 function makeNew(counter)
 {
@@ -35,41 +72,3 @@ function PlacePipes()
     setInterval(timer = function(timer){timer -= 200; return timer;}, 10000);
     setTimeout(function(){setInterval(makeNew(counter), timer)}, 3000);
 }
-
-document.addEventListener('DOMContentLoaded', () => {
-    const bird = document.querySelector('.bird');
-    const gameDisplay = document.querySelector('.game-container');
-    const ground = document.querySelector('.ground');
-
-    const gravity = -150   // gravitational acceleration
-    let velocity = 0;      // vertical velocity
-    let initTime = Date.now()
-    const interval = 20    // milliseconds per update
-    
-    let gameStarted = false;
-
-    let birdBottom = 100;
-    // let gravity = 2;
-
-    function startGame() {
-        let curTime = Date.now();
-        let timeDiff = (curTime - initTime)/1000
-        if (birdBottom > 0 && gameStarted) birdBottom += (velocity * timeDiff) + (gravity * timeDiff**2)/2
-        bird.style.bottom = birdBottom + 'px';
-        PlacePipes() // Bori
-        // if (birdBottom === 0) break;
-        document.addEventListener('keydown', jump);
-    }
-    let timerId = setInterval(startGame, interval);
-
-
-    function jump(e) {
-        if (e.key == ' ') {
-            if (!gameStarted) gameStarted = true;
-
-            initTime = Date.now();
-            velocity = 40;
-            bird.style.bottom = birdBottom + 'px';
-        }
-    }
-})
