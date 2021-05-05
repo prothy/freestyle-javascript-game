e921ea42b093f102f5a32913ffd444a7fe2e8427
HuBori <hunyadyb2000@gmail.com>
2021-05-05 11:25:08 +0200

    code for collision checking written (calling unsucessful)

--- FlappyBird/game.js
+++ FlappyBird/game.js
@@ -39,12 +39,12 @@ function PlacePipes()
 document.addEventListener('DOMContentLoaded', () => {
     const bird = document.querySelector('.bird');
     const gameDisplay = document.querySelector('.game-container');
-    const ground = document.querySelector('.ground');
+    // const ground = document.querySelector('.ground');
 
-    const gravity = -150   // gravitational acceleration
+    const gravity = -150;   // gravitational acceleration
     let velocity = 0;
-    let initTime = Date.now()
-    const interval = 20   // milliseconds per update
+    let initTime = Date.now();
+    const interval = 20;   // milliseconds per update
 
     let birdLeft = 220;
     let birdBottom = 100;
@@ -52,21 +52,63 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function startGame() {
         let curTime = Date.now();
-        let timeDiff = (curTime - initTime)/1000
-        if (birdBottom > 0) birdBottom += (velocity * timeDiff) + (gravity * timeDiff**2)/2
+        let timeDiff = (curTime - initTime)/1000;
+        if (birdBottom > 0) birdBottom += (velocity * timeDiff) + (gravity * timeDiff**2)/2;
         bird.style.bottom = birdBottom + 'px';
         bird.style.left = birdLeft + 'px';
-        PlacePipes() // Bori
+        PlacePipes(); // Bori
         // if (birdBottom === 0) break;
         document.addEventListener('keydown', jump);
     }
     let timerId = setInterval(startGame, interval);
+    let checker = setInterval(CheckCollision, interval);
 
+
+    function GameOver()
+    {
+        alert("Game Over!\nYou lost!");
+    }
 
     function jump() {
         initTime = Date.now();
         velocity = 40;
         bird.style.bottom = birdBottom + 'px';
         // timeDiff = 
+    }
+
+    function CheckCollision()
+    {
+        const obstacles = document.querySelectorAll('.obstacle');
+        const bird_pos = document.querySelector('.bird').getBoundingClientRect();
+
+        for (let obs of obstacles)
+        {
+            var rect = obs.getBoundingClientRect();
+            if (Compare(bird_pos, rect))
+            {
+                // TODO: write function GameOver()
+                GameOver();
+            }
+        }
+
+        function Compare(bird, obstacle)
+        {
+            /*
+            Azért használok 2 betűs változókat mert különben nem férne ki az egész feltétel a képernyőre.
+            A változónevek az objektum (bird/obstacle) első betűjéből
+            és a kulcs (top/bottom/right/left) első betűjéből tevődnek össze.
+            Ebből az oldalból indultam ki:
+            https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
+             */
+
+            var bt = bird["top"], bb = bird['bottom'], bl = bird['left'], br = bird['right'];
+            var ot = obstacle['top'], ob = obstacle['bottom'], ol = obstacle['left'], or = obstacle['right'];
+            
+            if ((bt <= ot) && (bt >= ob) && (bl >= ol) && (bl <= or)) return true;
+            else if ((bb <= ot) && (bb >= ob) && (br <= ol) && (bl >= or)) return true;
+            else if ((bt <= ot) && (bt >= ob) && (br <= ol) && (bl >= or)) return true;
+            else if ((bb <= ot) && (bb >= ob) && (bl >= ol) && (bl <= or)) return true;
+            else return false;
+        }
     }
 })
