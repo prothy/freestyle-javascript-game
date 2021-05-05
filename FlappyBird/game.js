6598c74bbea1ca7790e1fcf9b1eb6a25b7ab7be9
prothy <lukacsurgay@gmail.com>
2021-05-05 22:34:16 +0200

    added collision and game over

--- FlappyBird/game.js
+++ FlappyBird/game.js
@@ -23,7 +23,9 @@ function startGame() {
 
     let gameStarted = false;
 
-    let birdBottom = 100;
+    let birdInitPos = 100;
+    let birdBottom = birdInitPos;
+
 
     function gameLoop() {
         let curTime = Date.now();
@@ -36,13 +38,54 @@ function startGame() {
         document.addEventListener('keydown', jump);
 
         if (gameStarted) {
-            CheckCollision();
-            if ((startTime - curTime) % 2000 === 0) {
+            console.log(startTime-curTime)
+            if ((startTime - curTime) % 1000 === 0) {
                 wrapper.appendChild(new Obstacle());
             }
+            checkCollision();
+            if (birdBottom <= 0) gameOver()
         }
     }
     let timerId = setInterval(gameLoop, interval);
+
+
+    function gameOver()
+    {
+        gameStarted = false;
+        alert("Game Over!\nYou lost!");
+        birdBottom = birdInitPos;
+    }
+
+    function checkCollision()
+    {
+        const obstacles = document.querySelectorAll('.obstacle');
+        const bird_pos = document.querySelector('.bird');
+
+        for (let obs of obstacles)
+        {
+            if (compare(bird_pos, obs)) {
+                // TODO: write function GameOver()
+                console.log("Van egy talalat.");
+                gameOver();
+            }
+        }
+    }
+
+
+    function compare(bird, obstacle)
+    {
+        const bt = bird.getBoundingClientRect().top;
+        const bb = bird.getBoundingClientRect().bottom;
+        const bl = bird.getBoundingClientRect().left;
+        const br = bird.getBoundingClientRect().right;
+
+        const ot = obstacle.getBoundingClientRect().top;
+        const ob = obstacle.getBoundingClientRect().bottom;
+        const ol = obstacle.getBoundingClientRect().left;
+        const or = obstacle.getBoundingClientRect().right;
+
+        return (!(bb < ot || br < ol || bl > or || bt > ob ));
+    }
 
 
     function jump(e) {
@@ -86,94 +129,7 @@ function startGame() {
                 transform: ['translateX(0px)', `translateX(-500px)`]
             }, 2000)
             
+            // obstacle.remove();
         }
     }
 }
-
-function GameOver()
-{
-    alert("Game Over!\nYou lost!");
-}
-
-function CheckCollision()
-{
-    const obstacles = document.querySelectorAll('.obstacle');
-    const bird_pos = document.querySelector('.bird');
-
-    for (let obs of obstacles)
-    {
-        if (Compare(bird_pos, obs) === true)
-        {
-            // TODO: write function GameOver()
-            console.log("Van egy talalat.");
-            GameOver();
-        }
-    }
-}
-
-
-function Compare(bird, obstacle)
-{
-
-    /*
-    Azért használok 2 betűs változókat mert különben nem férne ki az egész feltétel a képernyőre.
-    A változónevek az objektum (bird/obstacle) első betűjéből
-    és a kulcs (top/bottom/right/left) első betűjéből tevődnek össze.
-    Ebből az oldalból indultam ki:
-    https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
-     */
-
-    const bt = bird.getBoundingClientRect().top;
-    const bb = bird.getBoundingClientRect().bottom;
-    const bl = bird.getBoundingClientRect().left;
-    const br = bird.getBoundingClientRect().right;
-
-    const ot = obstacle.getBoundingClientRect().top;
-    const ob = obstacle.getBoundingClientRect().bottom;
-    const ol = obstacle.getBoundingClientRect().left;
-    const or = obstacle.getBoundingClientRect().right;
-
-    let result = false;
-
-    if ((bt <= ot) && (bt >= ob) && (bl >= ol) && (bl <= or)) {result = true;}
-    else if ((bb <= ot) && (bb >= ob) && (br <= ol) && (bl >= or)) {result = true;}
-    else if ((bt <= ot) && (bt >= ob) && (br <= ol) && (bl >= or)) {result = true;}
-    else if ((bb <= ot) && (bb >= ob) && (bl >= ol) && (bl <= or)) {result = true;}
-
-    return result;
-}
-/*
-function Compare_these() {
-    function getPositions( elem ) {
-        var pos, width, height;
-        pos = elem.position();
-        width = elem.clientWidth() / 2;
-        height = elem.clientHeight();
-        return [ [ pos.left, pos.left + width ], [ pos.top, pos.top + height ] ];
-    }
-
-    function comparePositions( p1, p2 ) {
-        var r1, r2;
-        r1 = p1[0] < p2[0] ? p1 : p2;
-        r2 = p1[0] < p2[0] ? p2 : p1;
-        return r1[1] > r2[0] || r1[0] === r2[0];
-    }
-
-    return function ( a, b ) {
-        var pos1 = getPositions( a ),
-            pos2 = getPositions( b );
-        console.log("Start to compare"); // idáig nem jut el a program.
-        if (comparePositions( pos1[0], pos2[0] ) && comparePositions( pos1[1], pos2[1] ))
-        {
-            console.log("The return value of Compare is true.");
-            return true;
-        }
-        else
-        {
-            console.log("The return value of Compare is false.");
-            return false;
-        }
-        //return (comparePositions( pos1[0], pos2[0] ) && comparePositions( pos1[1], pos2[1] ));
-    };
-}
-*/
